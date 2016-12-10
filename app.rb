328e101bb161d7fa1758fa52603778c17157c524
Lilly Piri <lilly@lillypiri.com>
2016-12-10 10:18:15 +1000

    working grrrreat

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
+require 'erb'
+require 'yaml/store'
 
 get '/' do
-  'Magic 8 Ball'
-
   erb :index
+end
+
+
+get '/answer' do
+  'Magic 8 Ball'
+  answers = []
+  @store = YAML::Store.new 'app.yml'
+  @store.transaction do
+    @store["answers"].each do |key, value|
+      value.times do
+        answers << key
+      end
+    end
+  end
+
+  @answer = answers.sample
+  erb :answer
 end
